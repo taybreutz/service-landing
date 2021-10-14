9729eb51435b432fb21baa0ff13ec9b0bc156e7c
taybreutz <taybreutz@gmail.com>
2021-10-14 11:50:30 -0400

    ..

---
+++ components/contract.js
@@ -0,0 +1,9 @@
+const Contract = () => {
+    return ( 
+    <div className="contract-container">
+        <div className="paper-contract">Hello</div>
+    </div> 
+    );
+}
+ 
+export default Contract;
